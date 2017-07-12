880f35b20612f9a7451d26aaaa2e867608c7a2ec
Thiago Rodrigues <thiago.rodrigues@inventsoftware.com.br>
2017-07-11 23:04:13 -0300

    Criando promeiro projeto AspNet MVC

---
+++ PortalDeNoticias/PortalDeNoticias/Controllers/NoticiasController.cs
@@ -0,0 +1,32 @@
+﻿using PortalDeNoticias.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortalDeNoticias.Controllers
+{
+    public class NoticiasController : Controller
+    {
+        // GET: Noticias
+        public ActionResult Index()
+        {                                                 
+            return View(RepositorioDeNoticias.Noticias);
+        }
+        public ActionResult Detalhes(int id)
+        {
+            var noticias = RepositorioDeNoticias.Noticias
+                                 .Where(c => c.Id == id).FirstOrDefault();
+            return View(noticias);
+        }
+        public ActionResult Buscar(string texto)
+        {
+            var noticias = RepositorioDeNoticias.Noticias
+                                 .Where(c => c.Titulo.Contains(texto) 
+                                          || c.Conteudo.Contains(texto)).ToList();
+            return View(noticias);
+        }
+
+    }
+}
---
+++ PortalDeNoticias/PortalDeNoticias/Models/RepositorioDeNoticias.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PortalDeNoticias.Models
+{
+    public class RepositorioDeNoticias
+    {
+        private static List<Noticia> noticias;
+        public static List<Noticia> Noticias
+        {
+            get
+            {
+                if (noticias == null)
+                    CriarNoticia();
+                return noticias;
+            }
+        }
+
+        private static void CriarNoticia()
+        {
+            noticias = new List<Noticia>();
+
+            noticias.Add(new Noticia
+            {
+                Id = 1,
+                Titulo = "Primeira noticia",
+                Autor = "Thiago",
+                Data = DateTime.Now,
+                Conteudo = "Lorem ipsum dolor sit amet, " +
+                "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
+            });
+
+            noticias.Add(new Noticia
+            {
+                Id = 2,
+                Titulo = "Segunda noticia",
+                Autor = "Thiago",
+                Data = DateTime.Now,
+                Conteudo = "Lorem ipsum dolor sit amet, " +
+                "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
+            });
+            noticias.Add(new Noticia
+            {
+                Id = 3,
+                Titulo = "Terceira noticia",
+                Autor = "Thiago",
+                Data = DateTime.Now,
+                Conteudo = "Lorem ipsum dolor sit amet, " +
+                "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
+            });
+            noticias.Add(new Noticia
+            {
+                Id = 4,
+                Titulo = "Quarta noticia",
+                Autor = "Thiago",
+                Data = DateTime.Now,
+                Conteudo = "Lorem ipsum dolor sit amet, " +
+                "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
+            });
+            noticias.Add(new Noticia
+            {
+                Id = 5,
+                Titulo = "Quinta noticia",
+                Autor = "Thiago",
+                Data = DateTime.Now,
+                Conteudo = "Lorem ipsum dolor sit amet, " +
+                "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
+            });
+        }
+    }
+}                                                          
